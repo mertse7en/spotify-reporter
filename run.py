5d7f5d58a7f7ff4cca738cee60b5361264f4ddab
Mert Seven <mertse7en@hotmail.com>
2021-11-20 15:17:23 +0300

    Add argparser to run  multiple job

--- run.py
+++ run.py
@@ -1,18 +1,42 @@
+import argparse
 import os
 from dotenv import load_dotenv
-from src.spotify_etl import SpotifyETL
+from src.api_manager import APIManager
+
 
 def load_environment() -> None:
     if os.path.exists("config/config.env"):
         load_dotenv(dotenv_path="config/config.env")
     else:
         raise FileNotFoundError("\"config/config.env\" not found")
-    
+
+
+def load_arg():
+    # Initialize arg parser
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--option", help="help")
+    args = arg_parser.parse_args()
+    run_option = args.option
+    return run_option
+
 
 def main() -> None:
-    spotify_etl = SpotifyETL()
-    spotify_etl.etl()
+    load_environment()
+    run_option = load_arg
+
+    api_manager = APIManager()
+
+    if run_option == 'daily_push':
+        api_manager.get_recently_played_df()
+
+    elif run_option == 'weekly_report':
+        # Weekly report
+        pass    
+
+
+
+
+
 
 if __name__ == "__main__":
-    load_environment()
     main()
